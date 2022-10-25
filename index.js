f44222f2f6bc0268a166bbf152c49db4054e9d55
rubayetseason <season.ss2001@gmail.com>
2022-10-25 23:33:16 +0600

    courses respond sent

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const courses = require('./data/courses.json');
 
-
-app.get('/' , (req, res) => {
+app.get('/', (req, res) => {
     res.send('Litfeather api running');
 });
 
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
 
 app.listen(port, () => {
     console.log('litfeather server running');
